6f65761e489e5af500d723e23e921425f8c16311
simonzack <simonzack@email.com>
2014-03-28 04:43:22 +1100

    Revert "encode msg before encrypting" (not the actual error)
    This reverts commit fe43acc7df254345d0406b2d76c418a3c8078a33.

--- pyfishcrypt.py
+++ pyfishcrypt.py
@@ -995,9 +995,10 @@ class XChatCrypt:
         else:
             decrypt_clz = Blowfish
             decrypt_func = blowcrypt_unpack
-        msg = msg.encode()
         try:
             b = decrypt_clz(key.key)
+            #if msg[-2:-1] == " ":
+            #   msg = msg[:-2]
             ret = decrypt_func(msg, b)
         except MalformedError:
             try:
