ef2c495480967a5b8632a436e426162b2211f526
simonzack <simonzack@email.com>
2014-03-30 03:43:49 +1100

    re-wrote dh1080.py

--- hexfish/dh1080.py
+++ hexfish/dh1080.py
@@ -8,122 +8,124 @@ import os
 import hashlib
 from .crypto import MalformedError, int_to_bytes, bytes_to_int
 
-g_dh1080 = 2
-p_dh1080 = int(
-    'FBE1022E23D213E8ACFA9AE8B9DFAD'
-    'A3EA6B7AC7A7B7E95AB5EB2DF85892'
-    '1FEADE95E6AC7BE7DE6ADBAB8A783E'
-    '7AF7A7FA6A2B7BEB1E72EAE2B72F9F'
-    'A2BFB2A2EFBEFAC868BADB3E828FA8'
-    'BADFADA3E4CC1BE7E8AFE85E9698A7'
-    '83EB68FA07A77AB6AD7BEB618ACF9C'
-    'A2897EB28A6189EFA07AB99A8A7FA9'
-    'AE299EFA7BA66DEAFEFBEFBF0B7D8B',
-    16
-)
-q_dh1080 = (p_dh1080 - 1) // 2
+class DH1080:
+    g = 2
+    p = int(
+        'FBE1022E23D213E8ACFA9AE8B9DFAD'
+        'A3EA6B7AC7A7B7E95AB5EB2DF85892'
+        '1FEADE95E6AC7BE7DE6ADBAB8A783E'
+        '7AF7A7FA6A2B7BEB1E72EAE2B72F9F'
+        'A2BFB2A2EFBEFAC868BADB3E828FA8'
+        'BADFADA3E4CC1BE7E8AFE85E9698A7'
+        '83EB68FA07A77AB6AD7BEB618ACF9C'
+        'A2897EB28A6189EFA07AB99A8A7FA9'
+        'AE299EFA7BA66DEAFEFBEFBF0B7D8B',
+        16
+    )
+    q = (p - 1) // 2
 
-def dh1080_b64encode(s):
-    '''
-    utf-7 base64 encode without padding characters, padding instead with 0 bits
-    '''
-    res = base64.b64encode(s)
-    if b'=' in res:
-        return res.rstrip(b'=')
-    else:
-        return res + b'='
-
-def dh1080_b64decode(s):
-    '''
-    utf-7 base64 encode without padding characters, padding instead with 0 bits
-    '''
-    # remove trailing 'A' if it's just for padding
-    if len(s)%4==1:
-        s = s[:-1]
-    # add padding characters
-    s += b'='*((-len(s))%4)
-    return base64.b64decode(s)
-
-def dh_validate_public(public, q, p):
-    """See RFC 2631 section 2.1.5."""
-    return 1 == pow(public, q, p)
-
-
-class DH1080Ctx:
-    """DH1080 context."""
     def __init__(self):
         self.public = 0
         self.private = 0
         self.secret = 0
+        # whether we are in the middle of a handshake
         self.state = 0
+        self.cbc = None
+
+        g, p, q = self.g, self.p, self.q
 
         bits = 1080
         while True:
             self.private = bytes_to_int(os.urandom(bits//8))
-            self.public = pow(g_dh1080, self.private, p_dh1080)
-            if 2 <= self.public <= p_dh1080 - 1 and \
-               dh_validate_public(self.public, q_dh1080, p_dh1080) == 1:
+            self.public = pow(g, self.private, p)
+            if 2 <= self.public <= p - 1 and self.validate_public_key(self.public, q, p) == 1:
                 break
 
-def dh1080_pack(ctx):
-    """
-    Uses strings as designed to be human readable in irc.
-    """
-    if ctx.state == 0:
-        ctx.state = 1
-        cmd = "DH1080_INIT"
-    else:
-        cmd = "DH1080_FINISH"
-    return "%s %s" % (cmd,dh1080_b64encode(int_to_bytes(ctx.public)))
+    @staticmethod
+    def b64encode(s):
+        '''
+        utf-7 base64 encode without padding characters, padding instead with 0 bits
+        '''
+        res = base64.b64encode(s)
+        if b'=' in res:
+            return res.rstrip(b'=')
+        else:
+            return res + b'='
 
-def dh1080_unpack(msg, ctx):
-    if not msg.startswith("DH1080_"):
-        raise ValueError
+    @staticmethod
+    def b64decode(s):
+        '''
+        utf-7 base64 encode without padding characters, padding instead with 0 bits
+        '''
+        # remove trailing 'A' if it's just for padding
+        if len(s)%4==1:
+            s = s[:-1]
+        # add padding characters
+        s += b'='*((-len(s))%4)
+        return base64.b64decode(s)
 
-    invalidmsg = "Key does not conform to RFC 2631. This check is not performed by any DH1080 implementation, so we use the key anyway. See RFC 2631 & RFC 2785 for more details."
+    def validate_public_key(self, public_key):
+        '''
+        See RFC 2631 section 2.1.5.
+        '''
+        return 1 < public_key < self.p
 
-    if ctx.state == 0:
-        if not msg.startswith("DH1080_INIT "):
-            raise MalformedError
-        ctx.state = 1
-        try:
-            cmd, public_raw = msg.split(' ', 1)
-            public = bytes_to_int(dh1080_b64decode(public_raw))
+    def validate_public_key_strict(self, public_key):
+        '''
+        See RFC 2631 section 2.1.5.
+        '''
+        return pow(public_key, self.q, self.p) == 1
 
-            if not 1 < public < p_dh1080:
-                raise MalformedError
+    def pack(self, cmd, key, cbc):
+        res = '{} {}'.format(cmd, self.b64encode(int_to_bytes(key)))
+        if cbc:
+            res += ' CBC'
+        return res
 
-            if not dh_validate_public(public, q_dh1080, p_dh1080):
-                print(invalidmsg)
-                pass
+    def unpack(self, msg):
+        words = msg.split()
+        if len(words) not in (2, 3):
+            raise ValueError('msg')
+        cbc = False
+        if len(words) == 3:
+            if words[-1] == 'CBC':
+                cbc = True
+        cmd = words[0]
+        if cmd not in ('DH1080_INIT', 'DH1080_FINISH'):
+            raise ValueError('msg')
+        key = self.b64decode(bytes_to_int(msg[1]))
+        return cmd, key, cbc
 
-            ctx.secret = pow(public, ctx.private, p_dh1080)
-        except:
-            raise MalformedError
+    def pack_request(self, cbc):
+        if self.state != 0:
+            raise ValueError('state')
+        return self.pack('DH1080_INIT', self.public, cbc)
 
-    elif ctx.state == 1:
-        if not msg.startswith("DH1080_FINISH "):
-            raise MalformedError
-        ctx.state = 1
-        try:
-            cmd, public_raw = msg.split(' ', 1)
-            public = bytes_to_int(dh1080_b64decode(public_raw))
+    def pack_response(self):
+        if self.state != 1:
+            raise ValueError('state')
+        return self.pack('DH1080_INIT', self.public, self.cbc)
 
-            if not 1 < public < p_dh1080:
-                raise MalformedError
+    def unpack_any(self, msg):
+        cmd, cbc, public_key = self.unpack(msg)
+        if cbc != self.cbc:
+            raise ValueError('cbc request received a non-cbc response')
+        if cmd == 'DH1080_INIT':
+            if self.state != 0:
+                raise ValueError('state')
+        elif cmd == 'DH1080_FINISH':
+            if self.state != 1:
+                raise ValueError('state')
+        if not self.validate_public_key(public_key):
+            raise ValueError('invalid public key')
+        invalid_strict_msg = 'Key does not conform to RFC 2631. This check is not performed by any DH1080 implementation, so we use the key anyway. See RFC 2631 & RFC 2785 for more details.'
+        if not self.validate_public_key_strict(public_key):
+            print(invalid_strict_msg)
+        self.secret = pow(public_key, self.private, self.p)
+        self.state = 1
 
-            if not dh_validate_public(public, q_dh1080, p_dh1080):
-                print(invalidmsg)
-                pass
+    def show_secret(self):
+        if self.secret == 0:
+            raise ValueError
+        return self.b64encode(hashlib.sha256(int_to_bytes(self.secret)).digest())
 
-            ctx.secret = pow(public, ctx.private, p_dh1080)
-        except:
-            raise MalformedError
-
-    return True
-
-
-def dh1080_secret(ctx):
-    if ctx.secret == 0:
-        raise ValueError
-    return dh1080_b64encode(hashlib.sha256(int_to_bytes(ctx.secret)).digest())
