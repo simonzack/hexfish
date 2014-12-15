7d47267fcd6c47c5e3c1fd4680e34cc07fbcf3da
simonzack <simonzack@email.com>
2014-12-16 03:44:04 +1100

    use __xxxitem__ so we can assume the key is a tuple; added has & __delitem__; added find_deepest() to improve code re-use

--- hexfish/config.py
+++ hexfish/config.py
@@ -1,8 +1,10 @@
 
 import base64
+import itertools
 import json
 import os
 import uuid
+from collections import OrderedDict
 
 import xchat
 from Crypto.Cipher import Blowfish
@@ -14,16 +16,17 @@ class Config:
         # nick-specific settings (aliases don't matter)
         'nick': {
             # user-specified defaults
-            str(uuid.UUID(int=0)): {
-                'cipher': 'blowcrypt',
-                'cbc': True,
-                'cbc_force': False,
-                'active': True,
+            # ordered dict to pretty-print
+            str(uuid.UUID(int=0)): OrderedDict([
+                ('cipher', 'blowcrypt'),
+                ('cbc', True),
+                ('cbc_force', False),
+                ('active', True),
                 # protect against exchanging new keys
-                'protect_key': False,
+                ('protect_key', False),
                 # do not respond to key exchanges
-                'stealth': False,
-            },
+                ('stealth', False),
+            ]),
         },
         # each nick has a single alias to an id (aliases can possibly create a security problem)
         # nick is encoded using it's host mask
@@ -72,34 +75,61 @@ class Config:
         with open(path, 'w') as fp:
             json.dump(container, fp, indent=4)
 
-    def get(self, keys):
-        is_default = False
-        if keys[0] == 'nick' and len(keys) >= 2:
-            if keys[1] not in self.config.get('nick', {}):
-                keys = ('nick', str(uuid.UUID(int=0))) + keys[2:]
-                is_default = True
-        for config in (self.config, self.default_config):
+    def find_deepest(self, keys, allow_default=True):
+        keys_search = [keys]
+        if allow_default and len(keys) >= 2 and keys[0] == 'nick':
+            keys_search.append(('nick', str(uuid.UUID(int=0))) + keys[2:])
+        config_search = [self.config]
+        if allow_default:
+            config_search.append(self.default_config)
+        res = []
+        for keys, config in itertools.product(keys_search, config_search):
+            res = [(None, config)]
             for key in keys:
-                if key in config:
-                    config = config[key]
-                else:
-                    is_default = True
+                if key not in config:
                     break
+                config = config[key]
+                res.append((key, config))
             else:
-                return config, is_default
-        raise KeyError
+                return list(zip(*res))
+        return list(zip(*res))
 
-    def set(self, keys, value):
+    def has(self, *keys):
+        '''
+        Returns 2 if item exists, 1 if a default exists, 0 if it doesn't exist.
+        '''
+        find_keys, find_configs = self.find_deepest(keys)
+        if len(find_keys) < len(keys) + 1:
+            return 0
+        elif tuple(find_keys[1:]) != tuple(keys) or find_configs[0] == self.default_config:
+            return 1
+        else:
+            return 2
+
+    def __getitem__(self, keys):
+        find_keys, find_configs = self.find_deepest(keys)
+        if len(find_keys) < len(keys) + 1:
+            raise KeyError
+        return find_configs[-1]
+
+    def __setitem__(self, keys, value):
         # always set even if values are the same, so changes in default won't break the config
-        type_keys = keys
-        if keys[0] == 'nick' and len(keys) >= 2:
-            type_keys = ('nick', str(uuid.UUID(int=0))) + keys[2:]
-        default_config = self.default_config
+        self.__getitem__(keys)
         config = self.config
-        for key, type_key in zip(keys[:-1], type_keys):
-            default_config = default_config[type_key]
-            config = config.setdefault(key, type(default_config)())
+        for key in keys[:-1]:
+            config = config.setdefault(key, {})
         config[keys[-1]] = value
+
+    def __delitem__(self, keys):
+        find_keys, find_configs = self.find_deepest(keys)
+        if len(find_keys) < len(keys) + 1:
+            raise KeyError
+        find_configs[-2].pop(find_keys[-1])
+        prev_config = find_configs[-2]
+        for key, config in zip(find_keys[-2::-1], find_configs[-3::-1]):
+            if not prev_config:
+                config.pop(key)
+            config = prev_config
 
     def create_id(self):
         return str(uuid.uuid4())
--- hexfish/tests/test_config.py
+++ hexfish/tests/test_config.py
@@ -1,22 +1,43 @@
 
-import uuid
 import unittest
+import uuid
 
 from hexfish.config import Config
 
 
+# noinspection PyStatementEffect
 class TestConfig(unittest.TestCase):
+    def test_has(self):
+        config = Config()
+        id_ = config.create_id()
+        config['nick', id_, 'cipher'] = 'blowcrypt'
+        self.assertEqual(config.has('nick', config.create_id(), 'does_not_exist'), 0)
+        self.assertEqual(config.has('nick', config.create_id(), 'cipher'), 1)
+        self.assertEqual(config.has('nick', id_, 'cipher'), 2)
+
     def test_get(self):
         config = Config()
-        self.assertEqual(config.get(('nick', config.create_id(), 'cipher')), ('blowcrypt', True))
-        self.assertEqual(config.get(('nick_id', '*default')), (str(uuid.UUID(int=0)), True))
+        self.assertEqual(config['nick', config.create_id(), 'cipher'], 'blowcrypt')
+        self.assertEqual(config['nick_id', '*default'], str(uuid.UUID(int=0)))
         with self.assertRaises(KeyError):
-            config.get(('nick', config.create_id(), 'does_not_exist'))
+            config['nick', config.create_id(), 'does_not_exist']
         with self.assertRaises(KeyError):
-            config.get(('nick_id', 'does_not_exist'))
+            config['nick_id', 'does_not_exist']
 
     def test_set(self):
         config = Config()
         id_ = config.create_id()
-        config.set(('nick', id_, 'cipher'), 'blowcrypt')
+        config['nick', id_, 'cipher'] = 'blowcrypt'
         self.assertEqual(config.config, {'nick': {id_: {'cipher': 'blowcrypt'}}})
+        with self.assertRaises(KeyError):
+            config['nick', config.create_id(), 'long_key', 'long_key'] = 'value'
+
+    def test_del(self):
+        config = Config()
+        id_ = config.create_id()
+        config['nick', id_, 'cipher'] = 'blowcrypt'
+        del config['nick', id_, 'cipher']
+        self.assertEqual(config.config, {})
+        config = Config()
+        with self.assertRaises(KeyError):
+            del config['nick_id', 'does_not_exist']
