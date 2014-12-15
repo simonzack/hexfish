731e34a96673c88a329c604a5badb8fa07ebba3e
simonzack <simonzack@email.com>
2014-12-16 04:15:39 +1100

    updated xchat.py

--- hexfish/xchat.py
+++ hexfish/xchat.py
@@ -1,4 +1,23 @@
 
 '''
-Placeholder file for unit tests, use mock if anything is needed here.
+Placeholder file for unit tests.
 '''
+
+import os
+
+
+def get_info(name):
+    if name == 'configdir':
+        return os.getcwd()
+
+
+def hook_command(*args, **kwargs):
+    pass
+
+
+def hook_unload(*args, **kwargs):
+    pass
+
+
+def unhook(*args, **kwargs):
+    pass
